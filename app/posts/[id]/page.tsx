a9ad0b6777bb862283783e605e9f6916dca5b12b
Milaadmg023 <bagerim962@gmail.com>
2024-11-23 13:14:42 +0330

    fix : fixning params type for deployment

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,17 +2,9 @@ import { getPost } from "@/utils/fetch";
 import { PostType } from "@/app/page";
 //components
 import SinglePost from "@/components/singlePost";
-type ProductDetailsPageProp = {
-  params: ProductDetailsPageParam;
-};
 
-type ProductDetailsPageParam = {
-  id: string;
-};
-
-
-export default async function Post(props: ProductDetailsPageProp) {
-  const { id } = props.params;
+export default async function Post({ params }: { params: Promise<any> & {id:string}}) {
+  const {id}  = params
   const post: PostType = await getPost(id);
   return (
     <>
